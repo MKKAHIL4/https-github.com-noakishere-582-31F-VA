193915166c2f3e5e6ce7d832f0633ae9f973e65f
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-05-06 16:22:37 -0400

    Final work   class05/Exercise/script.js

--- class05/Exercise/script2.js
+++ class05/Exercise/script2.js
@@ -1,19 +1,42 @@
-const loadUserBtn = document.getElementById(load-user-btn);
- const status = document.getElementById("status");
- const output = document.getElementById("list");
 
- status.textContent = "Loading...";
- user.List.innerHtml = "";
+const userList = document.getElementById("list");
+const status = document.getElementById("status");
+const loadUserBtn = document.getElementById("load-user-btn");
+
+
+
+ 
+ loadUserBtn.addEventListener("click", () => {
+    status.textContent = "Loading....";
+    userList.innerHTML = "";
+ 
 
  const userFetch = fetch("https://jsonplaceholder.typicode.com/users");
 
- userFetch.then((reponse) => {
-    return Response.json();
- }).then((data) => {
-    for(let i=0; i< 5; i++) {
-        userList.innerHtml += `<li>${data[i].username}<li>`;
-    }
+ userFetch
+  .then((response) => {
+     return response.json();
  })
+
+ .then((data) => {
+    for(let i = 0; i < 5; i++) {
+        //option1
+        let li = document.createElement("li");
+        li.textContent = data[i].username;
+
+        output.appendChild(li);
+        userList.append( `<li>${data[i].username}<li>`);
+        //userList.innerHtml += `<li>${data[i].username}<li>`;
+      }
+
+    })
  .catch((error) => {
     console.log(error)
- })
+    });
+});
+//another approach
+// firstFiveUser.forEach(user => {
+// const li = document.createElement("li");
+// li.textContent = `${user.name} (${user.email})`;
+// userList.appendChild(li);
+//});
