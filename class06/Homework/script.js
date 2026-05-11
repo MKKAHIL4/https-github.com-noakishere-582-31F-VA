34e6330568f75249870d6f0d894533cf4af759f1
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-05-11 03:28:37 -0400

    creating spongebob by create elemnt and append child  in script js  homework  class06/Homework/script.js

--- class06/Homework/script.js
+++ class06/Homework/script.js
@@ -30,3 +30,67 @@ function showLoading() {
 
    statusArea.appendChild(alert);
 }
+
+//building the sponge bob using create element.
+function renderUserCard(userObj) {
+    userCardArea.innerHTML = "";
+
+    const card = document.getElementById("div");
+    card.className = "card spongebob-card shadow-lg";
+
+    const body = document.getElementById("div");
+    body.className = "card-body";
+//eyes
+    const eyes = document.getElementById("div");
+    eyes.className = "spongbob-eyes";
+
+    const eye1= document.getElementById("div");
+    eye1.className = "spongebob-eye";
+
+    const eye2= document.getElementById("div");
+    eye2.className = "spongebob-eye";
+
+    eyes.appendChild(eye1);
+    eyes.appendChild(eye2);
+//mouth
+    const mouth = document.getElementById("div");
+    mouth.className = "spongebob-mouth";
+
+    const teeth= document.getElementById("div");
+    teeth.className = "spongebob-teeth";
+
+    const tooth1= document.getElementById("div");
+    tooth1.className = "spongebob-tooth";
+
+    const tooth2= document.getElementById("div");
+    tooth2.className = "spongebob-tooth";
+
+    teeth.appendChild(tooth1);
+    teeth.appendChild(tooth2);
+
+    //title
+    const title = document.createElement("h5");
+    title.className = "card-title text-center mt-2 mb-3";
+    title.textContent = "Loaded User (from JSON)";
+
+    //listGroup[]
+    const list = document.createElement("ul");
+    title.className = "list-group list-group-flush mb-3";
+
+    function addItem(label, value){
+    const li = document.createElement("li");
+    li.className = "list-group-item d-flex justify-content-between";
+
+    const strong = document.createElement("strong");
+    strong.textContent = label;
+    
+    const span = document.createElement("span");
+    span.textContent = value;
+
+    li.appendChild(strong);
+    li.appendChild(span);
+    li.appendChild(li);
+}
+
+
+}
