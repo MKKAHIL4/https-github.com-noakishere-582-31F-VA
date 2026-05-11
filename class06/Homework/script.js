79748423235e9e5eb19d04a1a579bf179bf13379
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-05-11 03:07:34 -0400

    creating helpers in script js  homework  class06/Homework/script.js

--- class06/Homework/script.js
+++ class06/Homework/script.js
@@ -6,9 +6,27 @@ const userCardArea = document.getElementById("userCardArea");
 //helper to show status messages using boot strap alert
 
 function showStatus(message, type = "info") {
-    statusArea.innerHTML = ` 
-    <div class="alert alert-${type} py-2 mb-0" role="alert">
-    ${message}
-    </div>`;
+    statusArea.innerHTML = "";
+    const alert = document.getElementById("div");
+    alert.className = `alert alert-${type} py-2 mb-0`;
+    alert.textContent = message; 
+
+    statusArea.appendChild(alert);
 }
 
+function showLoading() {
+   statusArea.innerHTML = "";
+   const alert = document.createElement("div");
+   alert.className = "alert alert-warning d-flex align-items-center py-2 mb-0";
+
+   const spinner = document.createElement("div");
+   spinner.className = "spinner-border spineer-border-sm me-2";
+
+   const text = document.createElement("span");
+   text.textContent = "Loading JSON text from the API.....";
+
+   alert.appendChild(spinner);
+   alert.appendChild(text);
+
+   statusArea.appendChild(alert);
+}
