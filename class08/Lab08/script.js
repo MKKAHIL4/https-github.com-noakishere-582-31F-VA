7d86f5c8e4942e4f7cc91e0761905685a54c91ef
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-05-14 14:01:19 -0400

    ceating variable declarations  Class 08   Lab08/script.js

---
+++ class08/Lab08/script.js
@@ -0,0 +1,9 @@
+//Variable declaration
+const loadBtn = document.getElementById("loadBtn");
+const clearBtn = document.getElementById("clearBtn");
+const statusArea = document.getElementById("statusArea");
+const userContainer = document.getElementById("userContainer");
+const searchInput = document.getElementById("searchInput");
+
+let cachedPosts = {};
+let loadedUsers = [];
