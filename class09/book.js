b53a8339a5e96e2242293ad99f7171f67ca44d80
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-05-23 22:04:26 -0400

    created Book.js  for class09 index.html

---
+++ class09/book.js
@@ -0,0 +1,32 @@
+// Book class module
+
+export class Book {
+    constructor(title, author, available = true) {
+        this.author = author ;
+        this.title = title ;
+
+        this.available = available;
+    }
+
+displayInfo() {
+    return`${this.title} by ${this.author} | Available: ${this.available}`;
+}
+
+borrow(){
+    if(this.available) {
+        this.available = false;
+        return `${this.title} borrowed`;
+    }
+   return `${this.title} Already borrowed`;
+    }
+
+returnBook(){
+    this.available = true;
+    return `${this.title} returned`;
+}
+toggle() {
+    this.available = !this.available;
+    return `${this.title} ${this.available}`;
+
+}
+}
