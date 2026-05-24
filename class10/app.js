23c75308be07d9b8a5c10ce8eab3b7e652b94224
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-05-24 05:16:18 -0400

    creating  dom and history log  for class10 app.js

--- class10/app.js
+++ class10/app.js
@@ -21,3 +21,27 @@ Book.prototype.returnBook = function() {
     this.available = true;
     return `${this.title} has been returned`;
 };
+
+Book.prototype.toggleAvailability = function() {
+    this.available = !this.available
+    return `${this.title} availabilty ${this.available}`;
+};
+
+
+// data and info
+const books = [
+    new Book("CleanCode", "RobertC Martin"), 
+    new Book("Eloquen Java Script", "Marjin Haverbeke"), 
+    new Book("You Dont Know JS","Kyle Simon"), 
+    new Book("The Pragmatic Programmer", "Andrew Hunt" ), 
+
+]
+
+//dom
+const librarydiv = document.getElementbyId("library");
+const toast = document.getElementbyId("toast");
+const toastMessage = document.getElementbyId("toastMessage");
+const archiveLog= document.getElementbyId("archiveToast");
+
+//store history
+let logs [];
