2babf2bf89b6ad5411a525fc058b51c210079faa
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-05-28 14:25:56 -0400

    building step5: Adding click event, try & catch in  app.js  class12 app.js

--- class12/app.js
+++ class12/app.js
@@ -1,8 +1,10 @@
+//Step 3 : Connect DOM elements in JS
 const loadPostBtn = document.getElementById("load-post-btn");
 const postIdInput= document.getElementById("post-id-input");
 const status = document.getElementById("status");
 const output = document.getElementById("output");
 
+//Step4 : Validation function with cases
 function validatePostId(id) {
     if(typeof id !== "number" || isNaN(id)) {
         throw new Error("Post Id Must Be a Valid Number");
@@ -16,3 +18,37 @@ function validatePostId(id) {
          throw new Error("Post ID too Large! Try a number between 1 and 99.");
     }
 }
+//STEP5: aDDING CLICK EVENT, TRY & CATCH IN APP.JS CLASS 12 APP.JS
+loadPostBtn.addEventListener("click", () => {
+    try {
+        const id = Number(postIdInput.value);
+
+        validatePostId(id);
+
+        status.textContent = "Loading post ....";
+        output.innerHTML = "";
+
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        .then((response) => {
+            if(!response.ok){
+                throw new Error(`HTTP error: ${response.status}`);
+            }
+            return response.json();
+         })
+        .then((post) => {
+            output.innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+            `;
+            status.textContent = "POST Loaded Successfully.";
+        } )
+        .catch((error) => {
+            status.textContent = "Error:" +error.message;
+        });
+        }catch(error) {
+            status.textContent = "Validation Error: " + error.message;
+        }
+
+    });
+        
+       
