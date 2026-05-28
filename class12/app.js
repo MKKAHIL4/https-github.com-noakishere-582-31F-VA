3aedc1e1680f43829bbdc0da84ce8551ed88eb4f
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-05-28 13:59:41 -0400

    building step3: connect Dom element in js  app.js  class12 app.js

---
+++ class12/app.js
@@ -0,0 +1,4 @@
+const loadPostBtn = document.getElementById("load-post-btn");
+const postIdInput= document.getElementById("post-id-input");
+const status = document.getElementById("status");
+const output = document.getElementById("output");
