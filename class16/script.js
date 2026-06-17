dcdfa9de6057f419de275d9ba0102ddbf081d714
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-06-17 13:50:44 -0400

    class16 script.js .

--- class16/script.js
+++ class16/script.js
@@ -42,3 +42,72 @@ class HelloShadow extends HTMLElement {
 //we are still rendering the elemnt, but its internal straucture belongs to its own DOM
 
 customElements.define("helo-shadow", HellowShadow);
+
+//without shadow DOM:
+//-outisde page styles may change your component unexpectedly
+//- your components styles may leak into the rest of the page
+
+// with shadow DOM:
+//-internal styles stay scoped to the component
+//- component structure becomes more slef-contained!
+
+/*
+*Scoped styles inside Shadow DOM
+*/
+class Fancycard extends HTMLElement {
+    connectedCallback(){
+        const shadow = this.attachShadow({mode: "open"});
+
+        shadow.innerHTML= `
+        <style>
+            .card{
+                border: 2px solid purple;
+                padding 1rem;
+                background:#f1e3ff;
+            }
+            
+            h2{
+            color:purple;
+            margin-top: 0;}
+        </style>
+        <div class="card">
+            <h2>Shadow card</h2>
+            <p> This is scoped inside the component.</p>
+        </div>
+        `;
+    }
+}
+customElements.define("fancy-card", FancyCard);
+
+// **
+//* Using attributes with Shadow DOM
+//*/
+class PlayerCard extends HTMLElement {
+    connectedCallback() {
+        const shadow = this.attachShadow({ mode: "open" });
+
+        const name = this.getAttribute("name");
+        const goals = this.getAttribute("goals");
+        const imgUrl = this.getAttribute("image-url");
+
+        shadow. innerHTML =`
+            <style>
+            .card {
+                border: 1px solid #333;
+                padding: 1rem;
+                margin-bottom: 1rem;
+                background: O#111;
+                color: white;
+                border-radius: 8px;
+
+            </style>
+
+            <div class="card">
+                <h2>${name}</h2>
+                <p>Goals: ${goals}</h2>
+                <img src="${imgUr1}">
+            </div>
+`;
+    }
+}
+customElements.define("player-card", PlayerCard);
